75cf80342463a0fb6efa69bdb5a7c0b71c30da97
vikash1807 <vikashchahar2024@gmail.com>
2026-03-03 16:34:55 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,10 +5,18 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment the code below to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+
+    # Block untill we recieve an incoming connection
+    connection, address = server_socket.accept() 
+
+    print(f"accedpted connection from {address}")
+
+    # Read data
+    data = connection.recv(b"PING")
+
+    # send data to a connection
+    connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":

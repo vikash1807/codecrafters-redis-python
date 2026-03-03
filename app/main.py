d7b7874d5b38f073e5168bb8eaead2bb5c482af4
vikash1807 <vikashchahar2024@gmail.com>
2026-03-03 22:19:24 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,12 +8,13 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
     # Block untill we recieve an incoming connection
-    connection, address = server_socket.accept() 
+    while(True):
+        connection, address = server_socket.accept() 
 
-    print(f"accepted connection from {address}")
+        print(f"accepted connection from {address}")
 
-    # send response to the client
-    connection.sendall(b"+PONG\r\n")
+        # send response to the client
+        connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":

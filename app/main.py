8873e961d950a2c22274fdb1e8598b9a01e30a09
vikash1807 <vikashchahar2024@gmail.com>
2026-03-04 00:35:08 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,17 @@
 import socket  # noqa: F401
+import asyncio
+
+async def handle_client(connection):
+    while True:
+        BUFFER_SIZE_BYTES = 1024
+        data = connection.recv(BUFFER_SIZE_BYTES)
+
+        if data:
+            # send response to the client
+            connection.sendall(b"+PONG\r\n")
+        else:
+            # data is empty means connection is closed
+            break
 
 
 def main():
@@ -7,20 +20,12 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
-    while True:
-        # Block untill we recieve an incoming connection
-        connection, address = server_socket.accept() 
-        
-        while True:
-            BUFFER_SIZE_BYTES = 1024
-            data = connection.recv(BUFFER_SIZE_BYTES)
+    # Block untill we recieve an incoming connection
+    connection, address = server_socket.accept() 
+    
+    loop = asyncio.get_running_loop()
 
-            if data:
-                # send response to the client
-                connection.sendall(b"+PONG\r\n")
-            else:
-                # data is empty means connection is closed
-                break
+    loop.run_in_executor(handle_client, (connection))
 
 
 
